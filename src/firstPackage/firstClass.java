a5bfccef4f979c4cac640d259370d85432cb994f
KentseMonageng <kentsemonageng@gmail.com>
2022-02-10 18:46:37 +0200

    hello world script

---
+++ src/firstPackage/firstClass.java
@@ -0,0 +1,17 @@
+package firstPackage;
+
+public class firstClass 
+{
+	/* line 1 
+	 * line 2
+	 * line 3
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hi world this Kentse"); // this is a comment 
+		System.out.println("second line ");
+		
+	}
+
+}
